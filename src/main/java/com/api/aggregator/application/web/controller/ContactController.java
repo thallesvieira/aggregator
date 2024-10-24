759424b5386a77e84ebdc4269d80592fbdf38b93
Thalles <thalles_jacobs@hotmail.com>
2024-10-24 18:03:15 -0300

    update code

--- src/main/java/com/api/aggregator/application/web/controller/ContactController.java
+++ src/main/java/com/api/aggregator/application/web/controller/ContactController.java
@@ -1,17 +1,14 @@
 package com.api.aggregator.application.web.controller;
 
-import com.api.aggregator.domain.exception.ExceptionResponse;
 import com.api.aggregator.domain.model.contact.Contact;
 import com.api.aggregator.domain.service.IContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 
